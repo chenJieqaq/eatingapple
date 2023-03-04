b197344ad4b53df0c3c9470f8ee01ae61fffff82
chenjie <mopengdeng@gmail.com>
2023-03-04 15:36:20 +0800

    上传jar包1.2

--- enterprise_system/src/test/java/com/eatingapple/EnterpriseSystemApplicationTests.java
+++ enterprise_system/src/test/java/com/eatingapple/EnterpriseSystemApplicationTests.java
@@ -8,4 +8,13 @@ class EnterpriseSystemApplicationTests {
 
 
 
+    public static void main(String[] args) {
+        float f1= (float) 2.03;
+        float f2= (float) 2.33;
+        boolean aBoolean=(f2-f1==0.3);
+        float f3=f2-f1;
+        System.out.println(aBoolean);
+        System.out.println(f3);
+    }
+
 }
